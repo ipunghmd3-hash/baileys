79072b2facb05e5ec1e9a6fea32ca1e7a937ef68
ipunghmd3-hash <ipunghmd3@gmail.com>
2026-03-24 05:49:34 +0700

    Create postinstall.js

---
+++ postinstall.js
@@ -0,0 +1,8 @@
+console.log(`
+╔════════════════════════════════════════════════════╗
+║   Thanks for installing @ipunghmd3-hash/baileys   ║
+╠════════════════════════════════════════════════════╣
+║ Group   : https://chat.whatsapp.com/FFywa3JxadbH4glaofXBAq?mode=gi_t
+║ Channel : https://whatsapp.com/channel/0029VbC9AsB3wtazd1Pz2k1u
+╚════════════════════════════════════════════════════╝
+`)
